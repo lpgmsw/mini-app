f9773a80383a9a3c426ddf61cd2dc4a642431316
lpgmsw <leaningteexpc@gmail.com>
2019-05-06 22:11:13 +0900

    Branch_EditPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'blogs#index'
-  post "blogs/:blog_id" => "blogs#destroy"
-  resources :blogs, only: [:create, :new, :index, :destroy]
+  post 'blogs/:blog_id' => 'blogs#destroy'
+  get 'blogs/:blog_id/edit' => 'blogs#edit'
+  get 'blogs/:blog_id/update' => 'blogs#update'
+  resources :blogs, only: [:create, :new, :index, :destroy, :edit]
   resources :users, only: [:edit, :update]
 end
