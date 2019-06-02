909e51f8e079680909dc6878c68f7f42d366ac3f
lpgmsw <leaningteexpc@gmail.com>
2019-06-02 11:27:00 +0900

    Branch_AddAjax_Create_ViewSendForm

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post 'blogs/:blog_id' => 'blogs#destroy'
   get 'blogs/:blog_id/edit' => 'blogs#edit'
   patch 'blogs/:blog_id' => 'blogs#update'
-  resources :blogs, only: [:create, :new, :index, :destroy, :edit]
+  resources :blogs, only: [:create, :new, :index, :destroy, :edit, :update]
   resources :users, only: [:edit, :update]
 end
